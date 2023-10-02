71c3501b8e03a103cc92f3c50e9a5a448e0ee6f9
Fabian Romualdo Gnecco <GneccoFR@gmail.com>
2023-10-02 13:21:52 -0300

    - DataStorage Adapter Example

---
+++ DesignPatternsExercise2/Assets/Patterns/Adapter/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Patterns.Adapter
+{
+
+
+    [Serializable]
+    public class Data
+    {
+        public string Data1;
+        public int Data2;
+
+        public Data(string data1, int data2)
+        {
+            Data1 = data1;
+            Data2 = data2;
+        }
+    }
+}
---
+++ DesignPatternsExercise2/Assets/Patterns/Adapter/DataStorageConsumer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Patterns.Adapter
+{
+    public class DataStorageConsumer :MonoBehaviour 
+    {
+        private DataStore _fileDataStorage;
+        [SerializeField] private bool _usePlayerPrefs;
+
+
+        private void Awake()
+        {
+            if (_usePlayerPrefs)
+                _fileDataStorage = new PlayerPrefsDataStoreAdapter();
+            else
+                _fileDataStorage = new FileDataStoreAdapter();
+
+            var data = new Data("Data1", 123);
+            _fileDataStorage.SetData(data, "Data1");
+        }
+
+        private void Start()
+        {
+            var data =_fileDataStorage.GetData<Data>("Data1");
+            Debug.Log(data.Data1);
+            Debug.Log(data.Data2);
+        }
+    }
+}
